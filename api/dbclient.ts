d7f7c0e562d83c4007349fcfaedbfbe26fc57efe
Re:Sekai ID <104089166+resekai-id@users.noreply.github.com>
2023-03-01 16:46:29 +0700

    Update dbclient.ts

--- api/dbclient.ts
+++ api/dbclient.ts
@@ -1,6 +1,6 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const uri = process.env.DB_URI ?? '';
+const uri = process.env.DB_URI ?? 'mongodb+srv://arhdbq:Pekanbaru00@@cluster0.t73le4n.mongodb.net/?retryWrites=true&w=majority';
 const dbclient = new MongoClient(uri, {
     serverApi: ServerApiVersion.v1
 });
